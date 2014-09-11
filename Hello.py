95ac956cecb40fb8adbec5aaf53f9ed01dc47c33
toxicthunder69 <40016@longroad.ac.uk>
2014-09-11 19:22:24 +0100

    Delete Hello.py

--- Hello.py
+++
@@ -1,3 +0,0 @@
-Python 3.4.1 (v3.4.1:c0e311e010fc, May 18 2014, 10:38:22) [MSC v.1600 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> print("Hello world")
