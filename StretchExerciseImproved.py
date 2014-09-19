7434247cd9ac9d0cee28c5ccff839721e76f48a4
toxicthunder69 <40016@longroad.ac.uk>
2014-09-19 17:41:40 +0100

    1.3
    Added more customise-ability and condensed to one while loop, and
    bugfixes

--- StretchExerciseImproved.py
+++ StretchExerciseImproved.py
@@ -2,24 +2,41 @@
 #11/09/14
 #Stretch Exercise
 
-while :
-    print("Enter the measurements of you garden in meters")
-    leng = float(input("Length of your garden: "))
-    width = float(input("Width of your garden: "))
-    border_input = float(input("Enter the desired border size: "))
-    border = border_input * 2
-    turf = 10
-    
-    if leng <= border:
-        print("The length of your garden isn't long enough for a {0}m border.".format(border))
-        leng = float(input("Length of your garden: "))
-    if width <= border:
-        print("The width of your garden isn't long enough for a {0}m border.".format(border))
-        width = float(input("Width of your garden: "))
+leng = 0
+width = 0
+border_input = 0
+border = 0
+turf = 0
+leng_start = True
+width_start = True
+print("Enter the measurements of you garden in meters")
 
-turf_leng = leng - border
-turf_width = width - border
-area = turf_leng * turf_width
+while leng <= border or width <= border:
+    if leng <= border:
+        if leng_start == True:
+            leng = float(input("Length of your garden: "))
+            leng_start = False
+        else:
+            print("Your garden isn't long enough for a {0}m border around it.".format(border_input))
+            leng = float(input("Length of your garden: "))
+    if width <= border:
+        if width_start == True:
+            width = float(input("Width of your garden: "))
+            width_start = False
+        else:
+            print("Your garden isn't wide enough for a {0}m border around it.".format(border_input))
+            width = float(input("Width of your garden: "))
+    
+    if border == 0:
+        border_input = float(input("Enter the desired border size: "))
+        border = border_input * 2
+    else: break
+    if turf == 0:
+        turf = float(input("Enter price per sqrm of turf in £'s: "))
+    else: break
+
+area = (leng - border) * (width - border)
 cost = format(area * turf, '.2f')
 
-print("It will cost you £{0} to turf your garden.".format(cost))
+print("It will cost you £{0} to turf your garden if you have a {1}m border around it.".format(cost, border_input))
+
