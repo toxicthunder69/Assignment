41c2fc0706eed82ff639fea6edba877d65223b4f
toxicthunder69 <40016@longroad.ac.uk>
2014-09-19 17:47:09 +0100

    Subfolders
    Sorted everything

---
+++ Development exercises/AdditionProgram.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+num3 = int(input("Enter number 3: "))
+num4 = int(input("Enter number 4: "))
+ans = num1 + num2 + num3 + num4
+print(ans)
